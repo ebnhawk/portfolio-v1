8254f92c405c08aaa97f6722598e074d6970e960
Molly Hanes <mollyfhanes@gmail.com>
2018-06-07 18:10:25 -0500

    custom font

--- components/Contact.js
+++ components/Contact.js
@@ -6,15 +6,15 @@ const Contact = () => {
       <h1 id="contact-title" data-aos="fade-in" className="white display-4">
         Contact
       </h1>
-      <div id="icons-container" className="display-4 white">
+      <div id="icons-container" className="display-4 white-link">
         <a href="mailto:hello@mollyfhanes.com">
-          <i className="fas fa-envelope-square white" id="email" />
+          <i className="fas fa-envelope-square" id="email" />
         </a>
         <a href="https://www.linkedin.com/in/mollyfhanes/" target="new">
-          <i className="fab fa-linkedin white" id="linkedin" />
+          <i className="fab fa-linkedin" id="linkedin" />
         </a>
         <a href="https://github.com/ebnhawk" target="new">
-          <i className="fab fa-github white" id="github" />
+          <i className="fab fa-github" id="github" />
         </a>
       </div>
     </div>
